d4fa8212092f55afde0f40ef9a6f3a9639e26d8e
Quinn Stratton <qgstratton@gmail.com>
2017-09-21 11:39:06 -0500

    Added Python directory, and put code from 20170820 into it.

---
+++ Python/hello_nate.py
@@ -0,0 +1,11 @@
+# Welcome to programming Nate!
+# These are comments, which get ignored by the computer.
+
+# This will be executed by the computer.
+
+print("Hello, Nate")
+
+print("Hello world!")
+
+print("test")
+
---
+++ Python/sales.py
@@ -0,0 +1,11 @@
+# Setup
+
+sales = 0
+
+done = False # Boolean Value i.e. True/False
+
+# start asking for sales
+
+while (not done):
+    sale = float(input("Enter your sale: "))
+    
